c02c3df224acab817e0a97dc217e31542d55639d
XuYueming <XuYueming101108@outlook.com>
2024-05-18 08:01:13 +0800

    delete unused file

---
+++ English/YZH English Helper/main.py
@@ -0,0 +1,12 @@
+# This py file is the main file of English helper by Xu Yueming.
+"""@ By 徐跃鸣 @"""
+from IO import print, Fore, Back
+
+def main() -> None:
+    """
+    the main function
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
